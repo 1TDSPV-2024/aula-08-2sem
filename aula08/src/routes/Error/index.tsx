99b3dd8cbf8eaed80384f018619778062335b7ad
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-04 19:18:13 -0300

    Atualizando o projeto base

--- aula08/src/routes/Error/index.tsx
+++ aula08/src/routes/Error/index.tsx
@@ -1,13 +1,20 @@
 
 
-export default function Error(){
+export default function Error() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "Erro 404";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "Erro 404";
 
-    return(
+  const imgPathNumber = Math.floor(Math.random() * 3) + 1;
+
+  return (
+    <div>
+      <h1>Erro 404 - Página não encontrada.</h1>
       <div>
-        <h1>Olá, mundo sou o Error!</h1>
+        <figure>
+          <img src={`/img404/img-${imgPathNumber}-404.jpg`} alt="Erro-404" />
+        </figure>
       </div>
-    );
-  }
+    </div>
+  );
+}
--- aula08/src/routes/Produtos/index.tsx
+++ aula08/src/routes/Produtos/index.tsx
@@ -1,13 +1,43 @@
+import { Link } from "react-router-dom";
+import { listaProdutos } from "../../listaProdutos";
 
 
-export default function Produtos(){
+export default function Produtos() {
 
-      //MUDANDO O TÍTULO DA PÁGINA!!!
-      document.title = "PRODUTOS";
+  //MUDANDO O TÍTULO DA PÁGINA!!!
+  document.title = "PRODUTOS";
 
-    return(
-      <div>
-        <h1>Olá, mundo sou o Produtos!</h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Produtos Eletrônicos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* COMENTÁRIO */}
+          {listaProdutos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome}</td>
+              <td>{produto.qtd}</td>
+              <td>{produto.preco}</td>
+              <td><Link to={`/editar/produtos/${produto.id}`}>Editar</Link> </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>
+              Total de produtos : <span>{listaProdutos.length}</span>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
